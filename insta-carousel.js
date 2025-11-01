61c5f7dff8657eb3274fd43c92310a4c3a712b65
millapasta <106206192+millapasta@users.noreply.github.com>
2025-11-01 02:02:13 +0100

    update 01.11

--- insta-carousel.js
+++ insta-carousel.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   const carousels = document.querySelectorAll('.carousel');
-  
+
   carousels.forEach(carousel => {
     const track = carousel.querySelector('.carousel-track');
     const images = Array.from(track.children);
