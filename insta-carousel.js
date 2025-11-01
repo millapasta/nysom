1935a448bacc628515b752508b9967e72fef81f6
millapasta <106206192+millapasta@users.noreply.github.com>
2025-11-01 01:37:52 +0100

    insta-carousel.js

--- insta-carousel.js
+++ insta-carousel.js
@@ -1,22 +1,25 @@
-const carousels = document.querySelectorAll('.carousel');
-carousels.forEach(carousel => {
-  const track = carousel.querySelector('.carousel-track');
-  const images = Array.from(track.children);
-  const nextButton = carousel.querySelector('.next');
-  const prevButton = carousel.querySelector('.prev');
-  let index = 0;
+document.addEventListener("DOMContentLoaded", function() {
+  const carousels = document.querySelectorAll('.carousel');
+  
+  carousels.forEach(carousel => {
+    const track = carousel.querySelector('.carousel-track');
+    const images = Array.from(track.children);
+    const nextButton = carousel.querySelector('.next');
+    const prevButton = carousel.querySelector('.prev');
+    let index = 0;
 
-  function updateCarousel() {
-    track.style.transform = `translateX(-${index * 100}%)`;
-  }
+    function updateCarousel() {
+      track.style.transform = `translateX(-${index * 100}%)`;
+    }
 
-  nextButton.addEventListener('click', () => {
-    index = (index + 1) % images.length;
-    updateCarousel();
-  });
+    nextButton.addEventListener('click', () => {
+      index = (index + 1) % images.length;
+      updateCarousel();
+    });
 
-  prevButton.addEventListener('click', () => {
-    index = (index - 1 + images.length) % images.length;
-    updateCarousel();
+    prevButton.addEventListener('click', () => {
+      index = (index - 1 + images.length) % images.length;
+      updateCarousel();
+    });
   });
 });
